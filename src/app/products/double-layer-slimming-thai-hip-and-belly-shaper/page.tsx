c9a02b86b9c83c56a9d1c93eaf9bf764b0eb42d0
shourov-rahman <shourovju43@gmail.com>
2024-07-09 03:35:07 +0600

    feat: add hero section for double layer slimming thai and heap belly shaper

--- src/app/products/double-layer-slimming-thai-hip-and-belly-shaper/page.tsx
+++ src/app/products/double-layer-slimming-thai-hip-and-belly-shaper/page.tsx
@@ -1,3 +1,24 @@
-export default function DoubleLaterSlimmingPage() {
-  return <h1>Double Layer Slimming Thai Hip and Belly Shaper</h1>;
+export default function DoubleLayerSlimmingThaiAndHeapBellyShaper() {
+  return (
+    <div>
+      {/* section: hero */}
+      <section className="mb-6 sm:mb-8">
+        <div className="bg-gradient-to-r from-blue-700 to-[#B06AB3] font-sans px-6 py-12">
+          <div className="container mx-auto flex flex-col justify-center items-center text-center">
+            <h1 className="text-white sm:text-4xl text-3xl font-bold mb-4">
+              ডাবল লেয়ার স্লিমিং থাই, হিপ ও বেলী শেপার ১ পিস ৬৮০/- টাকা, ২ পিস
+              ১১৮০ টাকা মাত্র
+            </h1>
+
+            <button
+              type="button"
+              className="bg-white text-xl mt-6 text-blue-600 font-semibold py-3 px-6 rounded-lg hover:bg-slate-100"
+            >
+              অর্ডার করতে ক্লিক করুন
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
