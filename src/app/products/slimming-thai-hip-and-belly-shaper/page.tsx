68d23d8f1aaa36702b98450bcecec28108f60915
shourov-rahman <shourovju43@gmail.com>
2024-10-12 19:11:37 +0600

    fix : change route of double-layer-slimming-thai-hip-and-belly-shaper and fix list error
    - change double-layer-slimming-thai-hip-and-belly-shaper route to slimming-thai-hip-and-belly-shaper
    - change <li> component to <ul> component to solve list related error

--- src/app/products/slimming-thai-hip-and-belly-shaper/page.tsx
+++ src/app/products/slimming-thai-hip-and-belly-shaper/page.tsx
@@ -1,6 +1,6 @@
 import { CheckCircleIcon } from "@heroicons/react/24/outline";
 
-export default function DoubleLayerSlimmingThaiAndHeapBellyShaper() {
+export default function SlimmingThaiAndHeapBellyShaper() {
   return (
     <div>
       {/* section: hero */}
@@ -8,8 +8,8 @@ export default function DoubleLayerSlimmingThaiAndHeapBellyShaper() {
         <div className="bg-gradient-to-r from-blue-700 to-[#B06AB3] font-sans px-6 py-12">
           <div className="container mx-auto flex flex-col justify-center items-center text-center">
             <h1 className="text-white sm:text-4xl text-3xl font-bold mb-4">
-              ডাবল লেয়ার স্লিমিং থাই, হিপ ও বেলী শেপার ১ পিস ৬৮০/- টাকা, ২ পিস
-              ১১৮০ টাকা মাত্র
+              স্লিমিং থাই, হিপ ও বেলী শেপার ১ পিস ৬৮০/- টাকা, ২ পিস ১১৮০ টাকা
+              মাত্র
             </h1>
 
             <button
@@ -42,23 +42,23 @@ export default function DoubleLayerSlimmingThaiAndHeapBellyShaper() {
 
         <div className="md:flex px-4 md:space-x-2">
           <ul className="text-xl pt-6 flex flex-col gap-y-4">
-            <li className="flex gap-x-4">
+            <ul className="flex gap-x-4">
               <li>
                 <CheckCircleIcon className="h-10 w-10 text-blue-700" />
               </li>
               <li className="mt-1">
                 এটি ব্যাবহারে আপনাকে ইন্সট্যান্ট স্লিম দেখাবে
               </li>
-            </li>
-            <li className="flex gap-x-4">
+            </ul>
+            <ul className="flex gap-x-4">
               <li>
                 <CheckCircleIcon className="h-10 w-10 text-blue-700" />
               </li>
               <li className="mt-1">
                 থাই, হিপ ও পেটের মেদ কমিয়ে আপনাকে আকর্ষনীয় করে তুলবে
               </li>
-            </li>
-            <li className="flex gap-x-4">
+            </ul>
+            <ul className="flex gap-x-4">
               <li>
                 <CheckCircleIcon className="h-10 w-10 text-blue-700" />
               </li>
@@ -66,18 +66,18 @@ export default function DoubleLayerSlimmingThaiAndHeapBellyShaper() {
                 স্থাই ভাবে স্লিম করবে।ব্যবহারে আরামদায়ক তাই নরমাল পেন্টির মত
                 ব্যাবহার করতে পারবেন
               </li>
-            </li>
+            </ul>
           </ul>
           <ul className="text-xl pt-6 flex flex-col gap-y-4">
-            <li className="flex gap-x-4">
+            <ul className="flex gap-x-4">
               <li>
                 <CheckCircleIcon className="h-10 w-10 text-blue-700" />
               </li>
               <li className="mt-1">
                 সাধারন ভাবে ধোয়ার উপযোগী, কোন পার্শ্বপ্রতিক্রিয়া নেই
               </li>
-            </li>
-            <li className="flex gap-x-4">
+            </ul>
+            <ul className="flex gap-x-4">
               <li>
                 <CheckCircleIcon className="h-10 w-10 text-blue-700" />
               </li>
@@ -85,15 +85,13 @@ export default function DoubleLayerSlimmingThaiAndHeapBellyShaper() {
                 নিয়মিত ভাবে ৪৫-৬০ দিন ব্যাবহারে আপনার মেদ, ভুঁড়ি কমিয়ে আপনাকে
                 স্লিম ও আকোর্ষনীয় করবে
               </li>
-            </li>
-            <li className="flex gap-x-4">
+            </ul>
+            <ul className="flex gap-x-4">
               <li>
                 <CheckCircleIcon className="h-10 w-10 text-blue-700" />
               </li>
-              <li className="mt-1">
-                নয় হুকের অরিজিনাল পন্য তাই বডিতে সহজে ফিট হবে
-              </li>
-            </li>
+              <li className="mt-1">অরিজিনাল পন্য তাই বডিতে সহজে ফিট হবে</li>
+            </ul>
           </ul>
         </div>
       </section>
@@ -105,25 +103,25 @@ export default function DoubleLayerSlimmingThaiAndHeapBellyShaper() {
         </h2>
 
         <ul className="text-xl px-4 pt-6 flex flex-col gap-y-4">
-          <li className="flex gap-x-4">
+          <ul className="flex gap-x-4">
             <li>
               <CheckCircleIcon className="h-10 w-10 text-blue-700" />
             </li>
             <li className="mt-1">১০০% অরিজিনাল পন্য</li>
-          </li>
-          <li className="flex gap-x-4">
+          </ul>
+          <ul className="flex gap-x-4">
             <li>
               <CheckCircleIcon className="h-10 w-10 text-blue-700" />
             </li>
             <li className="mt-1">কোন অগ্রিম টাকা ছাড়া পন্য অর্ডার করুন</li>
-          </li>
-          <li className="flex gap-x-4">
+          </ul>
+          <ul className="flex gap-x-4">
             <li>
               <CheckCircleIcon className="h-10 w-10 text-blue-700" />
             </li>
             <li className="mt-1">পন্য রিসিভ করে মূল্য পরিশোধ করুন</li>
-          </li>
-          <li className="flex gap-x-4">
+          </ul>
+          <ul className="flex gap-x-4">
             <li>
               <CheckCircleIcon className="h-10 w-10 text-blue-700" />
             </li>
@@ -131,13 +129,13 @@ export default function DoubleLayerSlimmingThaiAndHeapBellyShaper() {
               কালার বা সাইজ জনিত যেকোন সমস্যায় সহজে রিটার্ন বা এক্সচেঞ্জ করার
               সুবিধা
             </li>
-          </li>
-          <li className="flex gap-x-4">
+          </ul>
+          <ul className="flex gap-x-4">
             <li>
               <CheckCircleIcon className="h-10 w-10 text-blue-700" />
             </li>
             <li className="mt-1">সঠিক সময়ে পন্য ডেলিভারির নিশ্চয়তা</li>
-          </li>
+          </ul>
         </ul>
       </section>
 
